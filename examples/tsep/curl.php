198bf312546f8e0117d5822eb9da2524e440155b
Shane Logsdon <shane@shanelogsdon.com>
2019-09-24 11:50:01 -0400

    add initial transit/tsep support

---
+++ examples/tsep/curl.php
@@ -0,0 +1,39 @@
+<?php
+
+return function (
+  $serviceUrl, 
+  $endpoint, 
+  $queryString, 
+  $requestHeaders, 
+  $data = null, 
+  $verb = 'POST', 
+  $contentType = 'application/json', 
+  $timeout = 65000
+) {
+  $request = curl_init($serviceUrl . $endpoint . $queryString);
+  $requestHeaders = array_merge($requestHeaders, [
+      'Content-Type' => sprintf('%s', $contentType),
+      'Content-Length' => $data === null ? 0 : strlen($data),
+  ]);
+  $headers = [];
+  foreach ($requestHeaders as $key => $value) {
+      $headers[] = $key . ': '. $value;
+  }
+  curl_setopt($request, CURLOPT_CONNECTTIMEOUT, $timeout);
+  curl_setopt($request, CURLOPT_TIMEOUT, $timeout);
+  curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false); //true,);
+  curl_setopt($request, CURLOPT_SSL_VERIFYHOST, false); //2,);
+  curl_setopt($request, CURLOPT_CUSTOMREQUEST, strtoupper($verb));
+  curl_setopt($request, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+  curl_setopt($request, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($request, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($request, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
+  curl_setopt($request, CURLOPT_VERBOSE, false);
+  
+  $curlResponse = curl_exec($request);
+  $curlInfo = curl_getinfo($request);
+  $curlError = curl_errno($request);
+
+  return [$curlResponse, $curlInfo, $curlError];
+};
---
+++ src/internal/gateways/tsep/index.ts
@@ -0,0 +1,47 @@
+import { IActions } from "..";
+import { options } from "../../lib/options";
+
+import actionNormalizeResponse from "./action-normalize-response";
+import actionTokenize from "./action-tokenize";
+import actionValidateData from "./action-validate-data";
+
+export const supports = {
+  apm: {
+    androidPay: false,
+    applePay: false,
+  },
+  consumerAuthentication: false,
+  eCheck: false,
+  gift: false,
+  tokenization: {
+    cardNotPresent: true,
+    cardPresent: false,
+    eCheck: false,
+    gift: false,
+  },
+};
+
+const domains = {
+  // Genius Checkout has an automatic sandbox feature for developer / partner accounts
+  production: "https://gateway.transit-pass.com",
+  sandbox: "https://stagegw.transnox.com",
+};
+
+export const urls = {
+  tokenization: (prod: boolean) => {
+    options.tsepHost = prod ? domains.production : domains.sandbox;
+    return `${options.tsepHost}/transit-tsep-web/jsView/${options.deviceId}?${options.manifest}`;
+  },
+};
+
+export const actions: IActions = {
+  normalizeResponse: actionNormalizeResponse,
+  tokenize: actionTokenize,
+  validateData: actionValidateData,
+};
+
+export const requiredSettings = ["deviceId", "manifest"];
+
+export const getEnv = () => {
+  return options.env || "production";
+};
