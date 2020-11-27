dcc9a852f6601c7550bf7b024d1cbd92075164a6
Shane Logsdon <shane@shanelogsdon.com>
2020-11-27 15:29:36 -0500

    gpld: convert to typescript. add project files

--- packages/globalpayments-js-loader/index.ts
+++ packages/globalpayments-js-loader/index.ts
@@ -1,36 +1,32 @@
-// @ts-check
-
 /**
  * Prevent multiple copies of the Global Payments JavaScript
  * library from being loaded by only using a single Promise.
- * 
- * @type {Promise<object>}
  */
-let libraryLoaded;
+let libraryLoaded: Promise<object>;
 
 /**
  * Loads the Global Payments JavaScript library asynchronously, using a
  * resolved `Promise` to signal that the library has loaded.
  *
- * @param {string} [url]
- * 
+ * @param url
+ *
  * @returns The resolved library
  */
-export function loadLibrary(url) {
+export function loadLibrary(url?: string) {
     if (libraryLoaded) {
         return libraryLoaded;
     }
 
     if (!url) {
-        url = 'https://js.globalpay.com/v1/globalpayments.js';
+        url = "https://js.globalpay.com/v1/globalpayments.js";
     }
 
     return libraryLoaded = new Promise((resolve) => {
-        const script = document.createElement('script');
+        const script = document.createElement("script");
         script.defer = true;
-        script.src = url || '';
+        script.src = url || "";
         script.onload = () => {
-            resolve((/** @type {any} */ (window)).GlobalPayments);
+            resolve((window as any).GlobalPayments);
         };
         document.body.appendChild(script);
     });
