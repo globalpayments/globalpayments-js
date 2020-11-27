40e730bbdb41740843386cb45a0f77110c21d397
Shane Logsdon <shane@shanelogsdon.com>
2020-11-27 12:57:06 -0500

    gpjs: update example page titles

--- packages/globalpayments-js/examples/transit/index.php
+++ packages/globalpayments-js/examples/transit/index.php
@@ -5,7 +5,7 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
-    <title>TransIT Examples</title>
+    <title>TransIT / TSEP Examples</title>
   </head>
   <body>
     <h1>Examples</h1>
